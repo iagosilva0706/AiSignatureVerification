37c56469c4c2b188c899753fd0da95ead12a2caa
disruptio <iago.silva.0706@gmail.com>
2025-07-16 23:46:29 +0100

    Add model download and suppress TensorFlow logs

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 from PIL import Image
 import numpy as np
 import tensorflow as tf
-import os
 import uuid
 import requests
 
+
 def download_model():
-    url = 'https://drive.google.com/file/d/1O6F8-dcxAe2jwjrBV6jZLqmKFw8WtaHl/view?usp=drive_link'  # <-- Replace this with your actual direct download link
+    url = 'https://drive.google.com/file/d/1O6F8-dcxAe2jwjrBV6jZLqmKFw8WtaHl/view?usp=drive_link'  # Replace with actual direct download URL
     local_filename = 'model.h5'
 
     if not os.path.exists(local_filename):
@@ -19,12 +22,14 @@ def download_model():
 
     return local_filename
 
+
 # Download and load model once
 model_path = download_model()
 model = tf.keras.models.load_model(model_path)
 
 # Initialize FastAPI
 app = FastAPI()
+
 
 @app.post("/signature-verify/")
 def signature_verify(signature_image: UploadFile = File(...), database_image: UploadFile = File(...)):
@@ -40,11 +45,13 @@ def signature_verify(signature_image: UploadFile = File(...), database_image: Up
         "result": verdict
     })
 
+
 def save_temp_file(file: UploadFile):
     temp_filename = f"/tmp/{uuid.uuid4()}.png"
     with open(temp_filename, "wb") as f:
         f.write(file.file.read())
     return temp_filename
+
 
 def preprocess(image_path):
     image = Image.open(image_path).convert('L')
@@ -52,6 +59,7 @@ def preprocess(image_path):
     image = np.array(image) / 255.0
     image = np.expand_dims(image, axis=(0, -1))
     return image
+
 
 def predict_similarity(image1_path, image2_path):
     img1 = preprocess(image1_path)
